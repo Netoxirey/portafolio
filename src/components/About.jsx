d0dc4e5c678816a073e3610c813d12edfad0cbd0
Netoxirey <ernestoserna94@gmail.com>
2023-08-09 15:31:57 -0600

    fixin apos

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,10 +11,11 @@ function About() {
           <Image src="/profile-image.png" alt="profile" width={300} height={300} className='rounded-full w-full' />
           <div>
           <p className="text-lg leading-relaxed font-bold">
-            Hello there! I'm <span>Ernesto Serna</span>, a passionate and dedicated full-stack developer with a strong foundation in both front-end and back-end technologies. With a keen eye for detail and a creative mindset, I specialize in crafting seamless digital experiences that blend functionality with aesthetics.
+            Hello there! I&apos;m <span>Ernesto Serna</span>, a passionate and dedicated full-stack developer with a strong foundation in both front-end and back-end technologies. With a keen eye for detail and a creative mindset, I specialize in crafting seamless digital experiences that blend functionality with aesthetics.
             <br /><br />
-            My journey into the world of web development began when I discovered my love for turning ideas into interactive and user-friendly websites. Through continuous learning and hands-on experience, I've honed my skills in HTML, CSS, JavaScript, and various front-end frameworks. On the back end, I thrive in creating robust server-side solutions using technologies like Node.js and databases like MongoDB.
-          </p>
+            My journey into the world of web development began when I discovered my love for turning ideas into interactive and user-friendly websites. Through continuous learning and hands-on experience, I&apos;ve honed my skills in HTML, CSS, JavaScript, and various front-end frameworks. On the back end, I thrive in creating robust server-side solutions using technologies like Node.js and databases like MongoDB.
+            </p>
+
           </div>
         </RevealList>
     </section>
